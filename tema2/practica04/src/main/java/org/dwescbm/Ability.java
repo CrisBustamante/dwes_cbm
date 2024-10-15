ae5c7cc6efd469977e2f24a36cf6ad64d5619a7c
Cris Bustamante <60176960+CrisBustamante@users.noreply.github.com>
2024-10-15 22:56:20 +0200

    Add files via upload

---
+++ tema2/practica04/src/main/java/org/dwescbm/Ability.java
@@ -0,0 +1,22 @@
+package org.dwescbm;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Ability {
+    private String name;
+    private String url;
+
+    @Override
+    public String toString() {
+        return "    Habilidad { " +
+                "NOMBRE: " + name + " | " +
+                "URL: " + url + " | " +
+                " }";
+    }
+}
