e7485585d07b94ad71518fe5d890b92bca03b042
CrisBustamante <xsyruk@gmail.com>
2024-11-28 09:03:13 +0100

    Ultima run practica 2

---
+++ tema3/Practica03_WebApp/src/main/java/org/dwescbm/practica03_webapp/Practica03WebAppApplication.java
@@ -0,0 +1,13 @@
+package org.dwescbm.practica03_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica03WebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practica03WebAppApplication.class, args);
+    }
+
+}
---
+++ tema3/Practica03_WebApp/src/test/java/org/dwescbm/practica03_webapp/Practica03WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dwescbm.practica03_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica03WebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
